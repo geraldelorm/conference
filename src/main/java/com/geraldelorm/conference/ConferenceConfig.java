ac55b68b9eec50a76e0240642d3e21068e94c393
Gerald Gbagbe <geraldelorm14@gmail.com>
2022-01-17 16:15:20 +0000

    configuration and setting up view resolver

---
+++ src/main/java/com/geraldelorm/conference/ConferenceConfig.java
@@ -0,0 +1,19 @@
+package com.geraldelorm.conference;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class ConferenceConfig {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setPrefix("/WEB-INF/jsp/");
+        bean.setSuffix(".jsp");
+        bean.setOrder(0);
+        return bean;
+    }
+}
