6c8113bf816ba0aad34da84533fd6ffabd756afa
Gerald Gbagbe <geraldelorm14@gmail.com>
2022-01-17 16:25:32 +0000

    resolve static file e.g pdf in the application with Resource handles

--- src/main/java/com/geraldelorm/conference/ConferenceConfig.java
+++ src/main/java/com/geraldelorm/conference/ConferenceConfig.java
@@ -3,10 +3,18 @@ package com.geraldelorm.conference;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-public class ConferenceConfig {
+public class ConferenceConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("/WEB-INF/pdf/");
+    }
 
     @Bean
     public ViewResolver viewResolver() {
